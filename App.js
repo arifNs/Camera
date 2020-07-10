9eab2866551440fbf7ab5d7728f20883d29bdcb6
MUHAMMAD NUR ARIF BIN NOOR SHAMSUDIN <clgpakf@gmail.com>
2020-07-10 22:49:19 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -11,11 +11,33 @@ class App extends Component {
           ref={ref => {
             this.camera = ref
           }}
+          /* Trying to insert the button, but it keep sending 
+          undefined is not an object error.
+          
+          style={styles.view}
+          aspect={Camera.constants.Aspect.fill}>
+            <Text 
+            style={styles.capture} 
+            onPress={this.takePicture.bind(this)}>
+              [CAPTURE_IMAGE]
+            </Text>*/
         />
       </View>
-    )
+    );
   }
 }
+/*takepicture funtion
+takePicture() {
+  const options = {}
+
+  this.camera.capture({metadata: options}).then((data) => {
+    console.log(data)
+  }).catch((error) => {
+    console.log(error)
+  })
+}
+}
+*/
 
 const styles = StyleSheet.create({
   container: {
@@ -25,4 +47,25 @@ const styles = StyleSheet.create({
   }
 })
 
+/*
+Styling for the capture button and other component
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  view: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: 'blue',
+    borderRadius: 12,
+    color: 'white',
+    padding: 13,
+    margin: 40
+  }
+});*/
 export default App
